6fb699e4ef232974807e081e9e53b0e89875f304
Wendy <wendyatsea@outlook.com>
2023-08-14 23:20:39 -0700

    Add more practices

--- src/main/java/com/dojo/dp/Solutions.java
+++ src/main/java/com/dojo/dp/Solutions.java
@@ -1,5 +1,8 @@
 package com.dojo.dp;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /* Dynamic Programming */
 public class Solutions {
     /**
@@ -26,5 +29,111 @@ public class Solutions {
             n_1 = tmp;
         }
         return n_1;
+    }
+
+    /**
+     * Given two integer arrays that represent the weights and profits of N items, 
+     * implement a function knapSack() that finds a subset of these items that will gives us 
+     * the maximum profit without their cumulative weight exceeding a given number capacity. 
+     * Each item can only be selected once, which means we either skip it or put it in the knapsack.
+     * 
+     * @param profits int[] The profit that can be gained by each piece of the jewelry
+     * @param weights int[] The weight of each piece of jewelry
+     * @param capacity int The maximum weight that the knapsack can hold
+     * @return int The maximum profit that can be returned 
+     */
+    public static int knapsack(int profits[], int[] weights, int capacity) {
+        if (profits.length == 0 || weights.length == 0 || capacity == 0) return 0;
+
+        if (profits.length != weights.length) return 0;  // invalid input
+
+        // For each item, calculate the profit and weight if it is included or excluded 
+        // in the knapsack and keep the maximum value
+        int[] dp = new int[capacity + 1]; 
+        for (int i = 0; i < profits.length; i++) {
+            for (int c = capacity; c >= weights[i]; c--) {
+                if (weights[i] <= c) {
+                    dp[c] = dp[c - weights[i]] + profits[i];
+                } else {
+                    dp[c] = Math.max(dp[c], profits[i]);
+                }
+            }
+        }
+        return dp[capacity];
+    }
+
+    /**
+     * Given an array of integers, write a function to find if any two subsets of the input array 
+     * exist such that the sum of both subsets is equal. You can assume that the array will only 
+     * consist of positive integers.
+     * 
+     * @param nums int[] a list of positive integers
+     * @return boolean True if can be partitioned.
+     */
+    public static boolean canPartition(int[] nums) {
+        if (nums.length == 0) return false;
+
+        int total = 0;
+        for (int n : nums) {
+            total += n;
+        }
+        if (total % 2 != 0) return false;
+
+        total = total / 2;  // the sum of the subarray
+
+        // An array to indicate is the number can be included in a subarray
+        boolean[] dp = new boolean[total + 1];
+        dp[0] = true; // can include 0 in the subarray
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = total; j >= nums[i]; j--) {
+                if (nums[i] <= j) {
+                    dp[j] = dp[j - nums[i]] || dp[j];
+                } 
+            }
+            if (dp[total]) return true;
+        }
+        return false;
+    }
+
+    /**
+     * Given two strings, s1 and s2, write a function that finds and 
+     * returns the length of the longest substring of s2 and s1 (if any exist).
+     * 
+     * @param s1
+     * @param s2
+     * @return int the length of the longest common string if exist; otherwise, return 0
+     */
+    public static int lcsLength(String s1, String s2) {
+        if (s1 == null || s1.isEmpty() || s2 == null || s2.isEmpty()) return 0;
+
+        int maxLength = 0;
+        HashMap<Character, ArrayList<Integer>> chars = new HashMap<>();
+        for (int i = 0; i < s1.length(); i++) {
+            char c = s1.charAt(i);
+            if (!chars.containsKey(c)) {
+                chars.put(c, new ArrayList<>());
+            }
+            chars.get(c).add(i);
+        }
+
+        for (int i = 0; i < s2.length(); i++) {
+            char c = s2.charAt(i);
+            if (!chars.containsKey(c)) continue;
+
+            for (int j : chars.get(c)) {
+                if (s1.length() - j < maxLength) continue;
+                
+                int a = 1;
+                while (i + a < s2.length() && j + a < s1.length() 
+                    && s1.charAt(j + a) == s2.charAt(i + a)) {
+                    a += 1;
+                }
+                maxLength = Math.max(maxLength, a);
+            }
+
+            if (maxLength > s2.length() - i) break;
+        }
+
+        return maxLength;
     }
 }
--- src/test/java/com/dojo/dp/SolutionsTests.java
+++ src/test/java/com/dojo/dp/SolutionsTests.java
@@ -22,4 +22,48 @@ public class SolutionsTests {
         Assertions.assertEquals(6765, value);
     }
 
+    @Test
+    public void testKnapsack() {
+        int[] profits = {1,6,10,16};
+        int[] weights = {1,2,3,5};
+        int profit = Solutions.knapsack(profits, weights, 7);
+        Assertions.assertEquals(22, profit);
+
+        profit = Solutions.knapsack(profits, weights, 6);
+        Assertions.assertEquals(17, profit);
+    }
+
+    @Test
+    public void testCanBePartitioned() {
+        int[] nums1 = {1,2,3,4};
+        boolean status = Solutions.canPartition(nums1);
+        Assertions.assertTrue(status);
+
+        int[] nums2 = {1,2,3,6,1,1};
+        status = Solutions.canPartition(nums2);
+        Assertions.assertTrue(status);
+    }
+
+    @Test
+    public void testCanNotBePartitioned() {
+        int[] nums1 = {1,2,3,4,1};
+        boolean status = Solutions.canPartition(nums1);
+        Assertions.assertFalse(status);
+
+        int[] nums2 = {2,4,4,2,2,4};
+        status = Solutions.canPartition(nums2);
+        Assertions.assertFalse(status);
+    }
+
+    @Test
+    public void testLCSLength() {
+        String[] s1 = {"www.educative.io/explore", "xyzabcdefgABCabcd", "aaa", "efgh", "aaaa"};
+        String[] s2 = {"educative.io/edpresso", "abcdorabcdefgh", "aa", "abc", "aaaa"};
+        int[] expectedLengths= {14, 7, 2, 0, 4};
+
+        for (int i = 0; i < s1.length; i++) {
+            int value = Solutions.lcsLength(s1[i], s2[i]);
+            Assertions.assertEquals(expectedLengths[i], value);
+        }
+    }
 }
